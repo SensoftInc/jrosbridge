2b0ce721b664093d6787359e348e24506417528c
Ant1Zykl0n Laptop <armin1.kramer@gmx.de>
2016-09-01 18:31:40 +0200

    android support

--- src/main/java/edu/wpi/rail/jrosbridge/callback/ConnectionCallback.java
+++ src/main/java/edu/wpi/rail/jrosbridge/callback/ConnectionCallback.java
@@ -1,7 +1,12 @@
 package edu.wpi.rail.jrosbridge.callback;
 
+import org.java_websocket.handshake.ServerHandshake;
+
 /**
  * Created by Ant1Zykl0n on 29.08.2016.
  */
 public interface ConnectionCallback {
+    void onOpen(ServerHandshake handshakedata);
+    void onClose(int code, String reason, boolean remote);
+    void onError(Exception ex);
 }
